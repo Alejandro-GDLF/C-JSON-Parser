a7ec19345a0bff3d11aa158f6d58ae9ece431d3c
Alejandro <alejandro.garcia.dlf@gmail.com>
2023-05-25 21:31:12 +0200

    Changed boolean type

--- c_json_parser.h
+++ c_json_parser.h
@@ -2,7 +2,8 @@
 #define _C_JSON_PARSER_H_
 
 // Types
-typedef enum {
+typedef enum
+{
     JSONObject,
     String,
     Integer,
@@ -12,30 +13,31 @@ typedef enum {
     Null
 } JSONType;
 
-typedef struct {
+typedef struct
+{
     char *name;
     JSONType type;
     void *value;
 } JSONEntry;
 
-typedef struct {
+typedef struct
+{
     JSONEntry *entries;
     size_t entries_length;
 } JSONObject;
 
-typedef JSONEntry* JSONRoot;
-
+typedef JSONEntry *JSONRoot;
 
 // Functions
-JSONRoot* parse_string(char* string_to_parse, size_t string_to_parse_length);
+JSONRoot *parse_string(char *string_to_parse, size_t string_to_parse_length);
 
-void* get_value(JSONObject* json_object, char* field_name, JSONType* value_type, void* error);
+void *get_value(JSONObject *json_object, char *field_name, JSONType *value_type, void *error);
 
 // Specified functions
-int* get_integer(JSONObject* json_object, char* field_name, void* error);
-float* get_float(JSONObject* json_object, char* field_name, void* error);
-bool* get_boolean(JSONObject* json_object, char* field_name, void* error);
-char* get_string(JSONObject* json_object, char* field_name, void* error);
-JSONObject* get_json_object(JSONObject* json_object, char* field_name, void* error);
+int *get_integer(JSONObject *json_object, char *field_name, void *error);
+float *get_float(JSONObject *json_object, char *field_name, void *error);
+char *get_boolean(JSONObject *json_object, char *field_name, void *error);
+char *get_string(JSONObject *json_object, char *field_name, void *error);
+JSONObject *get_json_object(JSONObject *json_object, char *field_name, void *error);
 
 #endif
